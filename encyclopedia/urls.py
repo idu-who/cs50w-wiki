048e763c4527e7ae1b7f6e40ae943451cdcedf55
idu-who <104561260+idu-who@users.noreply.github.com>
2024-07-01 23:55:05 +0400

    search
    Signed-off-by: idu-who <104561260+idu-who@users.noreply.github.com>

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("search/", views.search, name="search"),
+    path("search-results/<str:query>/", views.search_results, name="search_results"),
     path("wiki/<str:title>/", views.entry, name="entry"),
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,3 +1,6 @@
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.views.decorators.http import require_POST
 from django.shortcuts import render
 
 from . import util
@@ -6,6 +9,33 @@ from . import util
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
+    })
+
+
+@require_POST
+def search(request):
+    query = request.POST["q"].strip()
+
+    if query in util.list_entries():
+        return HttpResponseRedirect(
+            reverse("entry", kwargs={"title": query})
+        )
+    else:
+        return HttpResponseRedirect(
+            reverse("search_results", kwargs={"query": query})
+        )
+
+
+def search_results(request, query):
+    results = []
+
+    for entry in util.list_entries():
+        if entry.find(query) != -1:
+            results.append(entry)
+
+    return render(request, "encyclopedia/search_results.html", {
+        "query": query,
+        "results": results,
     })
 
 
